418b03381266e6c99c53b7e9caf2483b626c5694
Ashish0016op <raccoonop0016@gmail.com>
2023-05-03 17:01:11 +0530

    Add&delete commit

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
-
-function saveLocalStorage(e){
-    e.preventDefault();
-    let name=e.target.name.value;
-    let email=e.target.email.value;
-    let phoneNumber=e.target.phone_number.value;
-    let obj={
-        name,
-        email,
-        phoneNumber
-    }
-    localStorage.setItem(obj.email,JSON.stringify(obj));
-    showUserOnScreen(obj);
-
+let form=document.getElementById('user-list');
+let addData=document.getElementById('userDetails');
+form.addEventListener('submit',addDetails);
+function addDetails(e){
+  e.preventDefault();
+  let Name_user=document.getElementById('name').value;
+  let Email_user=document.getElementById('userEmail').value;
+  let Phone_user=document.getElementById('userNumber').value;
+  let li=document.createElement('li');
+  li.className='user-name';
+  li.appendChild(document.createTextNode(Name_user));
+  li.appendChild(document.createTextNode(" - "+Email_user));
+  li.appendChild(document.createTextNode(" - "+Phone_user));
+  let deleteBtn=document.createElement('button');
+  deleteBtn.className='delete';
+  deleteBtn.appendChild(document.createTextNode('delete'));
+  li.appendChild(deleteBtn);
+  addData.appendChild(li);
 }
-function showUserOnScreen(obj){
-    let pareentElm=document.getElementById('userInput');
-    let childElm=document.createElement('li');
-    childElm.textContent=obj.name +'-'+obj.email+'-'+obj.phoneNumber;
-    pareentElm.appendChild(childElm);
+addData.addEventListener('click',removeItem);
+function removeItem(e){
+    e.preventDefault();
+    //console.log(1);
+     if(e.target.classList.contains('delete')){
+            let li=e.target.parentElement;
+            addData.removeChild(li);
+    }
 }
